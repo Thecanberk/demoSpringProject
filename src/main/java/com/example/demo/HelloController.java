2a462ebbadf6e57467fa10393ffd59b10a5c30e5
Canberk <thecanberk@gmail.com>
2020-05-07 22:38:36 +0300

    *helloWorld api

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,20 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HelloController {
 
-        @RequestMapping("/")
-        public String index() {
-            return "Hello World";
-        }
+    @RequestMapping(value = "/helloWorld", method = RequestMethod.GET)
+    public String getHelloWorld() {
+
+        return "HelloWorld!";
+    }
 
 
 }
